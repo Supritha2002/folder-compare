e3d273e2f82acb2da432fdec30e6d05f85914292
V S Supritha <103561315+Supritha2002@users.noreply.github.com>
2023-03-13 21:33:28 +0530

    Update compare_folder.py

--- golden-compare/compare_folder.py
+++ golden-compare/compare_folder.py
@@ -42,5 +42,5 @@ def compare_folder_contents(directory_path, standard_file):
     missing_files = missingfileslist(standardfilelist, compared_files)
     return missing_files, additional_files
 
-if __name__=="__main__":
-    compare_folder_contents(sys.argv[1],sys.argv[2])
+# if __name__=="__main__":
+#     compare_folder_contents(sys.argv[1],sys.argv[2])
