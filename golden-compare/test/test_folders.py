7ed1f96abe19f9328fbeba4e9f93367a95e119c6
V S Supritha <103561315+Supritha2002@users.noreply.github.com>
2023-03-23 13:28:50 +0530

    Update test_folders.py

--- golden-compare/test/test_folders.py
+++ golden-compare/test/test_folders.py
@@ -12,9 +12,6 @@ class TestFolder(unittest.TestCase):
         missingfiles, _ = compare_folder_contents("folder", "standardfile.json")
         self.assertEqual(len(missingfiles), 0)
        
-     
-    def test_invalid_folder_path(self):
-        assertRaises(exception,compare_folder_contents("files","standardfile.json"))
 
 
 if __name__ == '__main__':
