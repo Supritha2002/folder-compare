8a07f512085d89d1035c62811572561455b31df7
V S Supritha <103561315+Supritha2002@users.noreply.github.com>
2023-03-23 15:08:05 +0530

    Update compare_folder.py

--- golden-compare/compare_folder.py
+++ golden-compare/compare_folder.py
@@ -27,21 +27,23 @@ def compared_n_additionalfiles(allfiles, standardfilelist):
             additional_files.append(filename)
     return compared_files, additional_files
 
-def isExistingPath(directory_path):
-    isExisting=os.path.exists(directory_path)
-    return isExisting
-    
-def compare_folder_contents(directory_path, standard_file):
-    standardfilelist = load_contents_of_standardfile(standard_file)
+
+def allfileslist(directory_path):
     allfiles = []
-    isExisting=isExistingPath(directory_path)
-    if(isExisting is False):
-        sys.exit()
     for root, dirnames, filenames in os.walk(directory_path):
         for filename in filenames:
             fullpath = os.path.join(root, filename)
             _, subpath = fullpath.split("/", 1)
             allfiles.append(subpath)
+     return allfiles
+
+    
+def compare_folder_contents(directory_path, standard_file):
+    standardfilelist = load_contents_of_standardfile(standard_file)
+    isExisting = os.path.exists(directory_path)
+    if(isExisting is False):
+        sys.exit()
+    allfiles = allfileslist(directory_path)
     compared_files, additional_files = compared_n_additionalfiles(allfiles, standardfilelist)
     missing_files = missingfileslist(standardfilelist, compared_files)
     return missing_files, additional_files
