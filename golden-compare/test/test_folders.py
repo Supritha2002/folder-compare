e62f9121ba1268f42e0f332ee658b627ace65efc
V S Supritha <103561315+Supritha2002@users.noreply.github.com>
2023-03-23 15:13:54 +0530

    Update test_folders.py

--- golden-compare/test/test_folders.py
+++ golden-compare/test/test_folders.py
@@ -7,17 +7,17 @@ class TestFolder(unittest.TestCase):
     def test_for_additional_files(self):
         _, additionalfiles = compare_folder_contents("folder_additional_files", "standardfile.json")
         self.assertTrue(len(additionalfiles) > 0)
-  
+
     def test_for_missing_files(self):  # checks for length of missing files is equal to zero
         missingfiles, _ = compare_folder_contents("folder", "standardfile.json")
         self.assertEqual(len(missingfiles), 0)
 
     def test_invalid_folder_path(self):
-#         compare_folder_contents("folder1", "standardfile.json")
+        # compare_folder_contents("folder1", "standardfile.json")
         self.assertRaises(SystemExit, compare_folder_contents, "folder1", "standardfile.json")
 
     def test_invalid_standard_path(self):
-#         compare_folder_contents("folder_missing_files", "standard.json")
+        # compare_folder_contents("folder_missing_files", "standard.json")
         self.assertRaises(FileNotFoundError, compare_folder_contents, "folder_missing_files", "standard.json")
 
 
